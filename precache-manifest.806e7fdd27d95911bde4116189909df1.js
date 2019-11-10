9fe62ac7f124c9208784152b2ea74f6e07ea0714
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2019-11-10 15:21:24 +0100

    Updates

---
+++ precache-manifest.806e7fdd27d95911bde4116189909df1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7be566f7a02e47c497de7dc8c4306ff",
+    "url": "/lgame/index.html"
+  },
+  {
+    "revision": "77127d5fb558a94fec0d",
+    "url": "/lgame/static/css/main.618b9e15.chunk.css"
+  },
+  {
+    "revision": "42804a3889385343745a",
+    "url": "/lgame/static/js/2.46044743.chunk.js"
+  },
+  {
+    "revision": "77127d5fb558a94fec0d",
+    "url": "/lgame/static/js/main.82f7f953.chunk.js"
+  },
+  {
+    "revision": "7e009e2cb1cf190c63de",
+    "url": "/lgame/static/js/runtime-main.450986d1.js"
+  }
+]);
